1005eaa2c536d594ba3ab45b011b2ca27fb4916e
Anoop Rawat <160840200+rawatanoop21@users.noreply.github.com>
2024-03-06 22:24:21 +0530

    Add files via upload

---
+++ assgno4.js
@@ -0,0 +1,32 @@
+//problem no.1
+//area of rectangle
+length = 15;
+breadth = 30;
+rectangleareacalculator = length*breadth
+console.log(rectangleareacalculator );
+
+
+
+//problem no. 3
+//compare two number
+ var firstone = 36;
+  var secondone = 50;
+
+ var numbercomparison = firstone==secondone;
+ console.log(numbercomparison);
+
+
+
+ //problem no.4
+ //voting eligibility check
+  var rawatage = 17
+var votingeligibilitychecker = rawatage >= 18 
+console.log(votingeligibilitychecker);
+
+
+//problem no.5
+//Shopping Budget Evaluation
+var shopping = 439000
+
+var budgetshoppingevaluation = shopping <= 70000
+console.log(budgetshoppingevaluation)
