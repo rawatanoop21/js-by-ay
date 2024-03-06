9c55a90e4bd1619470eeb320b1dc7d023535e268
Anoop Rawat <160840200+rawatanoop21@users.noreply.github.com>
2024-03-06 22:03:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+//question 1-  i.e addition of two numbers
+//let number a=10 b=20
+
+//var a = 10;
+//var b = 20;
+//var c = a+b;
+//console.log(c);
+
+//  question 2- calculate the square of a number 
+//var a = 58 ;
+//var b = a*a;
+//console.log(b);
+
+
+
+// problem 2 :  play with expression 
+//(a+b) *c  /d - e
+
+//var a = 5;
+//var b = 48;
+//var c = 75;
+//var d = 20;
+//var e = 25;
+//var f = (a+b)*c/d-e;
+//console.log(f);
+
+
+// problem 5: Write a JavaScript expression to calculate the area of a rectangle with a length
+//of 8 units and a width of 5 units
+
+//var l = 8;
+//var b = 5;
+//var area = l*b;
+//console.log(area);
+
+
+//Problem 5: Multiple of 5
+//Take a variable, store a number, and check if a number is a multiple of 5.
+ //var a = 50;
+ //var b = 5;
+ //var c = a/b;
+ //console.log(c);
+
+
+
+var x = 5;
+var y =10;
+if(y>x){
+    var outcome="hello rawat"
+    console.log(outcome);
+}
+for(var i=0; i<5; i++)
+{
+    console.log("ya this is rawat");
+}
+
+
+
