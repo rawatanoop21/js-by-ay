8b62b1c0ca46b5299198b1033494c2738c39e178
Anoop Rawat <160840200+rawatanoop21@users.noreply.github.com>
2024-03-07 22:30:42 +0530

    Add files via upload

---
+++ assgno5.js
@@ -0,0 +1,73 @@
+//Problem 1:
+//Write a JavaScript program that compares two variables and prints the larger
+//one?
+var newone = 23;
+ var newtwo = 32;
+
+ if(newone > newtwo){
+     console.log("newone is greater than newtwo")
+ }
+ else if(newtwo > newone){
+    console.log("newtwo is greater than newone")
+ }
+ else{
+    console.log("newone is equal to Newtwo")
+ }
+
+
+
+ //Problem 2:
+//Write a JavaScript program that assigns a value to a variable representing age.
+//If the age is less than 18, print "You are minor"; otherwise, print "You are an
+//adult".
+ var rohan = 16;
+if("rohan>18")
+{console.log("you are minor")
+}
+else if("rohan<18"){
+    console.log("you are an adult")
+}
+
+
+
+//Problem 3:
+//Develop a program in JavaScript that assigns a value to a variable and checks
+//whether it is divisible by 5, printing "Divisible by 5" or "Not divisible by 5"
+//accordingly.
+ var checkvalue = 45 ;
+ if (checkvalue/5){
+    console.log("divisible by 5")
+ }
+
+
+ //Problem 4:
+//Develop a program in JavaScript that assigns a value to a variable representing
+//a number and checks whether it is even or odd.
+
+
+var myNumber = 16; 
+
+
+if (myNumber % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+
+//Problem 5:
+//Write a JavaScript program that assigns a value to a variable representing a
+//number and checks whether it is positive. If it is positive, print "Positive";
+//otherwise, print "Not positive".
+
+
+let number = -98; 
+
+
+if (number > 0) {
+    console.log("Positive");
+} else {
+    console.log("Not positive");
+}
+
